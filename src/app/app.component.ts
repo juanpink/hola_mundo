96e7ca3f9634a209cd845e6375c3036d6959a07e
juanpink <jmiopin6@gmail.com>
2025-01-13 11:31:39 +0100

    primer ejemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HolaMundoComponent } from './components/hola-mundo/hola-mundo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HolaMundoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
