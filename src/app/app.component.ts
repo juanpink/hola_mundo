986d0cf70807317b27b058535e0dcbb64bf1237c
juanpink <jmiopin6@gmail.com>
2025-01-13 12:56:23 +0100

    creando componentes básicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
 import { HolaMundoComponent } from './components/hola-mundo/hola-mundo.component';
+import { PieComponent } from './components/pie/pie.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HolaMundoComponent],
+  imports: [RouterOutlet, CabeceraComponent, HolaMundoComponent, PieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/pie/pie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pie',
+  standalone: true,
+  imports: [],
+  template: '<h1>Pie de página</h1>',
+  styles: ['h1 { color: green; }']
+})
+export class PieComponent {
+
+}
